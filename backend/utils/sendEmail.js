c127c3532be016ea472e4e32475111074f0f5c46
Himanshu Poriya <hporiya06@gmail.com>
2026-09-10 17:36:16 +0530

    fixing mail service for production

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,11 +1,14 @@
 import nodemailer from "nodemailer";
 
 async function sendEmail({ to, subject, html }) {
+
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "://gmail.com",
+    port: 587,
+    secure: false, 
     auth: {
       user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASSWORD,
+      pass: process.env.EMAIL_PASSWORD, 
     },
   });
 
