653293873e67aaea832dab11e8b2b41acad215e7
Himanshu Poriya <hporiya06@gmail.com>
2026-09-13 14:54:56 +0530

    fixing bugs forlocation service

--- frontend/lib/api.js
+++ frontend/lib/api.js
@@ -1,10 +1,10 @@
-"use-client"
+"use client"
 import axios from "axios";
 
 const api = axios.create({
-    baseURL:process.env.NEXT_PUBLIC_API_URL ,
-    withCredentials: true
-})
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api",
+  withCredentials: true,
+});
 
 export function getInvoiceUrl(orderId) {
   const apiBase = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api";
